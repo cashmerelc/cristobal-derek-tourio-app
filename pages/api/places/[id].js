19d42f199ab3c8440f599ab0fe63a4bd5f724d19
Derek Myers <cashmerelc@gmail.com>
2024-06-04 10:45:58 +0200

    Read place details from database

---
+++ pages/api/places/[id].js
@@ -0,0 +1,36 @@
+import dbConnect from "../../../db/dbConnect.js";
+import Place from "../../../db/models/Place";
+import Comment from "../../../db/models/Comment";
+
+export default async function handler(request, response) {
+  const { id } = request.query;
+  console.log("id:", id);
+  await dbConnect();
+
+  if (!id) {
+    return;
+  }
+
+  if (request.method === "GET") {
+    const place = await Place.findById(id);
+    console.log(place);
+    if (!place) {
+      return response.status(404).json({ status: "Not found" });
+    }
+
+    response.status(200).json({ place: place });
+  }
+}
+
+// const place = Place.find((place) => place._id.$oid === id);
+// const comment = place?.comments;
+// const allCommentIds = comment?.map((comment) => comment.$oid) || [];
+// const comments = db_comments.filter((comment) =>
+//   allCommentIds.includes(comment._id.$oid)
+// );
+
+// if (!place) {
+//   return response.status(404).json({ status: "Not found" });
+// }
+
+// response.status(200).json({ place: place, comments: comments });
